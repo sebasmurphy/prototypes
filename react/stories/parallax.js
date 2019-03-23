24da1ac3325179593fb5783460dd179d8e206834
Sebastian Murphy <sebastianmurphy@gmail.com>
2019-03-23 13:59:31 -0500

    add react storyboard

---
+++ react/stories/parallax.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+// import { Button } from '@storybook/react/demo';
+import Parallax from '../parallax';
+
+storiesOf('Parallax', module).add('default', () => <Parallax>test</Parallax>);
